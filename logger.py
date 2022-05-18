61e775e441a46fa3bc346537c321833dba60da28
brokenfloppydisk <kylehuang06@gmail.com>
2022-05-18 10:12:01 -0700

    Format to conform to PEP8

--- logger.py
+++ logger.py
@@ -6,8 +6,9 @@ import time
 import keyboard
 
 class TableLogger:
-    __slots__ = "start_time", "current_values", "data_frame", "has_timestamp",\
-                "smart_dash", "logging_finished", "start_timestamp", "output_file"
+    __slots__ = ("start_time", "current_values", "data_frame", 
+                "has_timestamp", "smart_dash", "logging_finished", 
+                "start_timestamp", "output_file")
 
     def __init__(self):
         self.start_time = time.perf_counter()
@@ -22,36 +23,45 @@ class TableLogger:
         """ Update the current timestamp using perf_counter()
         """
         if not self.has_timestamp:
-            self.current_values["Timestamp"] = (time.perf_counter() - self.start_time,)
+            self.current_values["Timestamp"] = (time.perf_counter() 
+                                              - self.start_time,)
 
     def parse_args(self) -> argparse.Namespace:
         """ Parse command-line arguments
         """
-        parser = argparse.ArgumentParser(
-            description="Log the SmartDashboard entries on a NetworkTables server.")
+        parser = argparse.ArgumentParser(description="Log the SmartDashboard "
+                                                     "entries on a "
+                                                     "NetworkTables server.")
 
         parser.add_argument("IP", metavar="i", type=str, nargs=1,
-                            help="The IP or team number of the NetworkTables server.")
+                            help="The IP or team number of the "
+                            "NetworkTables server.")
         
-        parser.add_argument("directory", metavar="D", type=str, nargs="?", default="./",
-                            help="The directory where the csv file should be outputted. \
-                            Defaults to the current open directory.")
+        parser.add_argument("directory", metavar="D", type=str, 
+                            nargs="?", default="./",
+                            help="The directory where the csv file should"
+                            " be outputted. Defaults to the current"
+                            " open directory.")
         
-        parser.add_argument("check_time", metavar="T", type=int, nargs="?", default=50,
-                            help="The number of milliseconds between each log update. \
-                            Defaults to 50 milliseconds.")
+        parser.add_argument("check_time", metavar="T", type=int, 
+                            nargs="?", default=50,
+                            help="The number of milliseconds"
+                            " between each log update."
+                            " Defaults to 50 milliseconds.")
         
-        parser.add_argument("flush_time", metavar="F", type=float, nargs="?", default=3,
-                            help="The number of seconds between each log flush. \
-                            Defaults to 3 seconds.")
+        parser.add_argument("flush_time", metavar="F", type=float, 
+                            nargs="?", default=3,
+                            help="The number of seconds"
+                            " between each log flush."
+                            " Defaults to 3 seconds.")
         
         return parser.parse_args()
 
     def main(self):
         """ Main function of the logger. 
 
-        Initializes the NetworkTables client and gets values every 50 ms,
-        and then outputs the data to "SDlog_DATETIME.csv".
+            Initializes the NetworkTables client and gets values every 50 ms,
+            and then outputs the data to "SDlog_DATETIME.csv".
         """
         args = self.parse_args()
 
@@ -77,7 +87,8 @@ class TableLogger:
         self.current_values[key] = value
 
     def initialize_logger(self, ip: str, directory: str):
-        """ Initialize the NetworkTables client and update the initial current_values.
+        """ Initialize the NetworkTables client 
+            and update the initial current_values.
         """
         if len(ip) <= 4:
             ip = f"roborio-{ip}-frc.local"
@@ -102,24 +113,31 @@ class TableLogger:
         if output_directory[-1] != "/":
             output_directory += "/"
         
-        self.output_file = output_directory + "SDlog_" + time.strftime("%Y-%m-%d_%H:%M:%S") + ".csv"
+        self.output_file = (output_directory + "SDlog_" + 
+                           time.strftime("%Y-%m-%d_%H:%M:%S") + ".csv")
 
     def append_to_df(self) -> None:
-        """ Append all current values to the log dataframe and print status to the user.
+        """ Append all current values to the log dataframe 
+            and print the status to the user.
         """
         self.update_timestamp()
         curr_df = pd.DataFrame.from_dict(self.current_values, orient="columns")
         print("\n"*20
-            + "Now logging SmartDashboard values. Press SPACE to stop.\n"
+            + ("Now logging SmartDashboard values."
+               "Press SPACE to stop logging.\n"
+               "Press F to manually flush the log to a csv. "
+               "(occurs automatically every 3 seconds).\n")
             +f"Data on current frame:\n{curr_df}")
-        self.data_frame = pd.concat([self.data_frame, curr_df], ignore_index=True)
+        self.data_frame = pd.concat([self.data_frame, curr_df], 
+                                    ignore_index=True)
     
     async def log(self, frame_time: float, flush_time: float) -> None:
         """ Append values to the dataframe and print the status to the user.
         """
         async def check_keyboard() -> None:
             """ Check keyboard inputs every 20 ms.
-                Exit if the user presses space, and flush the log if the user presses f.
+                Exit if the user presses space, and 
+                flush the log if the user presses f.
             """
             flush_cooldown = 0.0
             while not self.logging_finished:
